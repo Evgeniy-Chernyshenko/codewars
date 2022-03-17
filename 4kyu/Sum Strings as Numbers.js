335d7feb9f70c24ee6db11ac58b493534c1b1452
Evgeniy-Chernyshenko <yevgeniyche@gmail.com>
2022-03-17 18:31:29 +0300

    Done kata

--- 4kyu/Sum Strings as Numbers.js
+++ 4kyu/Sum Strings as Numbers.js
@@ -1,9 +1,3 @@
 function sumStrings(a, b) {
-  // return (+a + +b).toLocaleString('fullwide', { useGrouping: false });
   return BigInt(a) + BigInt(b) + '';
 }
-
-console.log(
-  sumStrings('712569312664357328695151392', '8100824045303269669937')
-);
-// '712577413488402631964821329'
